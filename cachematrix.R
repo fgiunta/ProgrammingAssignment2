12352fbcea50eb783a62ccccf0e81fbff2b8bdd7
fgiunta <fgiunta1@gmail.com>
2015-09-22 20:54:45 -0500

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Demo functions to illustrate caching the result of lengthy calculation
+## makeCacheMatrix builds the feeder matrix and defines the active functions
+## cacheSolve determines whether the solve has been previously cached or must be
+## computed and then returns the value.
+## R programming class
+## 19Sep15
 
-## Write a short comment describing this function
+
+## feeder function, defining matrix manipulation functions
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+
+    set <- function(y){
+        x<<-y
+        m<<-NULL
+    }
+    get <- function()x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set=set, get=get, setinv=setinv,getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Demonstrate caching.  Check to see if solve exists in cache, if so return it
+## otherwise, compute the solve and cache it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+
+    ##check for cached solution
+    m<-x$getinv()
+    if(!is.null(m)){
+        ## exists, return pre-calc'd values
+        message("getting cached data...")
+        return(m)
+    }
+    ## no cacehd solution, pull matrix data and solve
+    data <- x$get()
+    m <- solve(data,...)
+
+    ## store calc'd value in cache
+    x$setinv(m)
+    m
 }
